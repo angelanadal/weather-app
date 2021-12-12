41076df844800b800cec1166ab5be86edd7c6d4b
nightingaele <84741089+nightingaele@users.noreply.github.com>
2021-12-11 20:45:48 -0600

    Updated API integration

--- src/index.js
+++ src/index.js
@@ -14,20 +14,28 @@ let day = days[currentTime.getDay()]; // returns a value between 0 and 6.
 let hours = currentTime.getHours();
 let minutes = String(currentTime.getMinutes()).padStart(2, "0");
 
-currentTime = `${day} ${hours}:${minutes}`;
+currentTime = `${day}, ${hours}:${minutes}`;
 
 let currentTimeText = document.querySelector("#current-time");
 currentTimeText.innerHTML = currentTime;
 
 let apiKey = "a8730d7b28118354d14e2046c817ba28";
 let units = "metric";
-let currentCityName = document.querySelector("#current-city");
 
 function handleWeatherResponse(response) {
-  let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = `${temperature}°C`;
-  currentCityName.innerHTML = response.data.name;
+  let cityElement = document.querySelector("#current-city");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windSpeedElement = document.querySelector("#current-wind-speed");
+  let feelsLikeElement = document.querySelector("#current-wind-chill");
+  let descriptionElement = document.querySelector("#description");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  feelsLikeElement.innerHTML = `${Math.round(response.data.main.feels_like)}°C`;
+  descriptionElement.innerHTML = `${response.data.weather[0].description}`;
 }
 
 function search(event) {
